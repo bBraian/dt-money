10b3eae99c0946de3cf7e3fffb1cfc357a9d8b1e
bBraian <braianvoficial@gmail.com>
2022-10-03 13:09:33 -0300

    configuring json server

--- src/pages/Transactions/index.jsx
+++ src/pages/Transactions/index.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
+    useEffect(() => {
+        fetch('http://localhost:3000/transactions')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }, [])
+
     return (
         <div>
             <Header />
