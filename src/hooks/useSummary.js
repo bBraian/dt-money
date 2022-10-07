ead278863a9630cc58b8b501d5d4c31d7031688f
bBraian <braianvoficial@gmail.com>
2022-10-06 22:26:14 -0300

    removing unused import

--- src/hooks/useSummary.js
+++ src/hooks/useSummary.js
@@ -1,4 +1,4 @@
-import { useContext, useMemo } from "react";
+import { useMemo } from "react";
 import { useContextSelector } from "use-context-selector";
 import { TransactionsContext } from "../contexts/TransactionsContext";
 
